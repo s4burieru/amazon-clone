968445259034b41ba8ef6c3b6b8b5c5620032695
s4vvvvv <sbrlzxc@gmail.com>
2025-12-16 19:32:28 +0800

    Update cart functionality to dynamically display cart quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -57,22 +57,30 @@ document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
     let matchingItem;
 
     cart.forEach((item) => {
-      if (productId === item) {
+      if (productId === item.productId) {
         matchingItem = item;
       }
     });
 
-    if (matchingItem === item) {
+    if (matchingItem) {
       matchingItem.quantity += 1;
     } else {
-      const productId = button.dataset.productId;
       cart.push({
         productId: productId,
         quantity: 1,
       });
     }
+
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+    console.log(cart);
   });
 });
