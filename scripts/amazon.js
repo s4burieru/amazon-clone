d1c5ee3bc267f350b4061c9971461b06bcaafbaa
s4vvvvv <sbrlzxc@gmail.com>
2025-12-16 20:04:27 +0800

    Enhance cart functionality to include product quantity selection

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -24,7 +24,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -58,6 +58,9 @@ document.querySelector(".js-products-grid").innerHTML = productsHTML;
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
+    const quantity = Number(
+      document.querySelector(`.js-quantity-selector-${productId}`).value
+    );
     let matchingItem;
 
     cart.forEach((item) => {
@@ -67,11 +70,11 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     });
 
     if (matchingItem) {
-      matchingItem.quantity += 1;
+      matchingItem.quantity += quantity;
     } else {
       cart.push({
         productId: productId,
-        quantity: 1,
+        quantity: quantity,
       });
     }
 
