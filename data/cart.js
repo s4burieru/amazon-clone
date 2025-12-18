c00208397fb82cb0a071ada90d008fd668449b28
s4burieru <sbrlzxc@gmail.com>
2025-12-18 18:32:29 +0800

    Refactor cart management by updating removeFromCart function and integrating it into checkout script

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -26,4 +26,16 @@ export function addToCart(productId, quantity) {
       quantity: quantity,
     });
   }
+}
+
+export function removeFromCart(productId) {
+  cart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      cart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 }
