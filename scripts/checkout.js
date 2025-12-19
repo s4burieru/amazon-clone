c7dde539be4aff5e1f440644d7d5c88b23e785ae
s4burieru <sbrlzxc@gmail.com>
2025-12-19 14:54:41 +0800

    Rename updateCartQuantity to updateQuantity for consistency in cart management

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import {
   cart,
   removeFromCart,
   calculateCartQuantity,
-  updateCartQuantity,
+  updateQuantity,
 } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
