64e81f470f209e799c3104f3883ff48d353e1796
s4burieru <sbrlzxc@gmail.com>
2025-12-29 17:47:06 +0800

    Add methods to Product class for retrieving star rating URL and formatted price

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
 export function getProduct(productId) {
   let matchingProduct;
 
@@ -23,6 +25,14 @@ class Product {
     this.name = productDetails.name;
     this.rating = productDetails.rating;
     this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+  return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+   return `$${formatCurrency(this.priceCents)}`;
   }
 }
 
