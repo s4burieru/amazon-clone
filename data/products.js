f45e7d8c5ab2250bc36320803dba9c4c5ccf1207
s4burieru <sbrlzxc@gmail.com>
2026-01-03 16:43:14 +0800

    Remove example Clothing instance from products.js

--- data/products.js
+++ data/products.js
@@ -42,19 +42,6 @@ class Clothing extends Product {
     <a href="${this.sizeChartLink}" target="_blank">Size Chart</a>`;
   }
 }
-const tshirt = new Clothing({
-  id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-  image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-  name: "Adults Plain Cotton T-Shirt - 2 Pack",
-  rating: {
-    stars: 4.5,
-    count: 56,
-  },
-  priceCents: 799,
-  keywords: ["tshirts", "apparel", "mens"],
-  type: "clothing",
-  sizeChartLink: "images/clothing-size-chart.png",
-});
 
 export const products = [
   {
