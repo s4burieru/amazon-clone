d2e2e66e5bc751d6ebe6da6627c542e8d571543b
s4burieru <sbrlzxc@gmail.com>
2026-01-04 20:44:45 +0800

    Format documents with prettier

--- data/cart-class.js
+++ data/cart-class.js
@@ -46,7 +46,6 @@ class Cart {
         quantity: quantity,
         deliveryOptionId: "1",
       });
-      
     }
     this.saveToStorage();
   }
--- data/cart-oop.js
+++ data/cart-oop.js
@@ -95,8 +95,8 @@ function Cart(localStorageKey) {
   return cart;
 }
 
-const cart = Cart('cart-oop');
-const businessCart = Cart('cart-business');
+const cart = Cart("cart-oop");
+const businessCart = Cart("cart-business");
 
 cart.loadFromStorage();
 
