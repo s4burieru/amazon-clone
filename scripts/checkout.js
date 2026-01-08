a9e5ba00c581ee0804ad5f320cd71b3074144f9e
s4burieru <sbrlzxc@gmail.com>
2026-01-08 17:38:41 +0800

    Refactor product loading to use Promise for rendering order and payment summaries

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,7 +4,18 @@ import { loadProducts } from "../data/products.js";
 //import "../data/backend-practice.js";
 //import "../data/cart-class.js";
 
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+/*
 loadProducts(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
