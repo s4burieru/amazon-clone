5cd74e064765f04d7c8f5a3bf4d9cbb3373e698a
s4burieru <sbrlzxc@gmail.com>
2026-01-08 17:56:27 +0800

    Implement loadCart function to fetch cart data from backend and update checkout process

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,21 +1,52 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import "../data/backend-practice.js";
 //import "../data/cart-class.js";
 
-new Promise((resolve) => {
-  loadProducts(() => {
-    resolve();
-  });
-}).then(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve("value1");
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  }),
+]).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
 
 /*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve("value1");
+  });
+})
+
+  .then((value) => {
+    return new Promise((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
+    });
+  })
+
+  .then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+  */
+
+/*
 loadProducts(() => {
-  renderOrderSummary();
-  renderPaymentSummary();
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
 });
 */
