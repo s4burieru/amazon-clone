32e08a1a2c2b5628e5f985fff7cb14c9ee2b12a2
s4burieru <sbrlzxc@gmail.com>
2026-01-10 19:45:45 +0800

    Used async await instead of promises and callbacks

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,21 @@ import { loadCart } from "../data/cart.js";
 //import "../data/backend-practice.js";
 //import "../data/cart-class.js";
 
+async function loadPage() {
+  await loadProductsFetch();
+
+  await new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
+
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
@@ -16,6 +31,7 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 
 /*
 new Promise((resolve) => {
