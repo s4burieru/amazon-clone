9bf36e3c8d72c96a1976c4e552dc2c0e919bcd31
s4burieru <sbrlzxc@gmail.com>
2026-01-10 19:54:45 +0800

    Handle errors in async await in checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,13 +6,18 @@ import { loadCart } from "../data/cart.js";
 //import "../data/cart-class.js";
 
 async function loadPage() {
-  await loadProductsFetch();
+  try {
+    await loadProductsFetch();
 
-  await new Promise((resolve) => {
-    loadCart(() => {
-      resolve();
+    await new Promise((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
     });
-  });
+    
+  } catch (error) {
+    console.log("Unexpected error. Please try again later.");
+  }
 
   renderOrderSummary();
   renderPaymentSummary();
