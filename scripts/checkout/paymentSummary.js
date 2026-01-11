427edc11b63ff218af61223b352a931748f2a782
s4burieru <sbrlzxc@gmail.com>
2026-01-11 18:48:35 +0800

    Implement error handling for order placement in payment summary

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -69,17 +69,23 @@ export function renderPaymentSummary() {
   document
     .querySelector(",js-place-order")
     .addEventListener("click", async () => {
-      const response = await fetch("https://supersimplebackend.dev/orders", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          cart: cart,
-        }),
-      });
+      try {
+        const response = await fetch("https://supersimplebackend.dev/orders", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            cart: cart,
+          }),
+        });
+        const order = await response.json();
+        addOrder(order);
+      } catch (error) {
+        console.log("Unexpected error. Please try again later.");
+      }
 
-      const order = await response.json();
-      addOrder(order);
+      window.location.href =
+        "/checkout/order-confirmation.html?orderId=" + order.id;
     });
 }
