44011ed1584816b1dd6ec408b54c8fe6ab93ab2b
s4burieru <sbrlzxc@gmail.com>
2026-01-14 17:43:57 +0800

    Add search functionality to products grid and update search input elements

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -7,7 +7,20 @@ loadProducts(renderProductsGrid);
 function renderProductsGrid() {
   let productsHTML = "";
 
-  products.forEach((product) => {
+  const url = new URL(window.location.href);
+  const search = url.searchParams.get("search");
+
+  let filteredProducts = products;
+
+  // If a search exists in the URL parameters,
+  // filter the products that match the search.
+  if (search) {
+    filteredProducts = products.filter((product) => {
+      return product.name.includes(search);
+    });
+  }
+
+  filteredProducts.forEach((product) => {
     productsHTML += `<div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
@@ -94,5 +107,10 @@ function renderProductsGrid() {
 
       addToCartAnimation(button);
     });
+  });
+
+  document.querySelector(".js-search-button").addEventListener("click", () => {
+    const search = document.querySelector(".js-search-bar").value;
+    window.location.href = `amazon.html?search=${search}`;
   });
 }
